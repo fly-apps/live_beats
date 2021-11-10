fdd37c5da866d6e28ae48ebfb44ec098692a234e
Chris McCord <chris@chrismccord.com>
2021-11-10 10:35:49 -0500

    Use GH LV and js

--- mix.exs
+++ mix.exs
@@ -39,7 +39,8 @@ defmodule LiveBeats.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, path: "~/oss/phoenix_live_view", override: true},
+      # {:phoenix_live_view, path: "~/oss/phoenix_live_view", override: true},
+      {:phoenix_live_view, github: "phoenixframework/phoenix_live_view", branch: "cm-sticky-live-render", override: true},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.5"},
       {:esbuild, "~> 0.2", runtime: Mix.env() == :dev},
@@ -50,7 +51,7 @@ defmodule LiveBeats.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:mint, "~> 1.0"},
-      {:heroicons, "~> 0.2.2"},
+      {:heroicons, "~> 0.2.2"}
     ]
   end
 
