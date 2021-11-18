80ed1dc96c5d2bc69214335931f34b2e0b5ede9c
Nolan Darilek <nolan@thewordnerd.info>
2021-11-18 14:00:43 -0600

    Use correct role for menu button.

--- lib/live_beats_web/views/layout_view.ex
+++ lib/live_beats_web/views/layout_view.ex
@@ -70,7 +70,8 @@ defmodule LiveBeatsWeb.LayoutView do
       <div>
         <button id={"#{@id}-menu"} type="button" phx-hook="Menu" data-active-class="bg-gray-100"
           class="group w-full bg-gray-100 rounded-md px-3.5 py-2 text-sm text-left font-medium text-gray-700 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-purple-500"
-          phx-click={show_dropdown("##{@id}-dropdown")}>
+          phx-click={show_dropdown("##{@id}-dropdown")}
+          aria-haspopup="true">
           <span class="flex w-full justify-between items-center">
             <span class="flex min-w-0 items-center justify-between space-x-3">
               <img class="w-10 h-10 bg-gray-300 rounded-full flex-shrink-0"
