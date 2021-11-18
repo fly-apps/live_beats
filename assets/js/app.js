be44f111d9dd17b62cea2134ba84e8f7ada05fe7
Nolan Darilek <nolan@thewordnerd.info>
2021-11-18 13:56:32 -0600

    Better keyboard management for dropdown menu.
    * Focus individual items when arrows are used so they're read by screen readers.
    * Wrap focus when arrowing up past first item, or down past last.
    * Return focus to the menu element when escape is pressed.
    * Block all other keys from reaching the app, so tab/left/right arrow don't move focus.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 import "phoenix_html"
-import {Socket} from "phoenix"
+import { Socket } from "phoenix"
 // import {LiveSocket} from "./phoenix_live_view"
-import {LiveSocket} from "phoenix_live_view"
+import { LiveSocket } from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 
 let nowSeconds = () => Math.round(Date.now() / 1000)
@@ -13,122 +13,123 @@ let execJS = (selector, attr) => {
 let Hooks = {}
 
 Hooks.Menu = {
-  getAttr(name){
+  getAttr(name) {
     let val = this.el.getAttribute(name)
-    if(val === null){ throw(new Error(`no ${name} attribute configured for menu`)) }
+    if (val === null) { throw (new Error(`no ${name} attribute configured for menu`)) }
     return val
   },
-  reset(){
+  reset() {
     this.activeClass = this.getAttr("data-active-class")
     this.deactivate(this.menuItems())
     this.activeItem = null
   },
-  mounted(){
+  mounted() {
     this.menuItemsContainer = document.querySelector(`[aria-labelledby="${this.el.id}"]`)
     this.reset()
     this.el.addEventListener("click", e => {
-      if(e.currentTarget.isSameNode(this.el)){
-        this.el.focus()
-        this.activate(0)
+      if (e.currentTarget.isSameNode(this.el)) {
+        requestAnimationFrame(() => this.activate(0))
       }
     })
-    this.el.addEventListener("keydown", e => {
-      if(e.key === "Escape"){
-        document.body.click()
-        this.reset()
-      } else if(e.key === "Enter" && !this.activeItem){
-        this.activate(0)
-      } else if(e.key === "Enter"){
-        this.activeItem.click()
-      }
-      if(e.key === "ArrowDown"){
+    document.addEventListener("keydown", e => {
+      if (this.activeItem) {
         e.preventDefault()
-        let menuItems = this.menuItems()
-        this.deactivate(menuItems)
-        this.activate(menuItems.indexOf(this.activeItem) + 1, menuItems.length - 1)
-      } else if(e.key === "ArrowUp"){
-        e.preventDefault()
-        let menuItems = this.menuItems()
-        this.deactivate(menuItems)
-        this.activate(menuItems.indexOf(this.activeItem) - 1, 0)
+        if (e.key === "Escape") {
+          document.body.click()
+          this.reset()
+          this.el.focus()
+        } else if (e.key === "Enter") {
+          this.activeItem.click()
+        } else if (e.key === "ArrowDown") {
+          let menuItems = this.menuItems()
+          let index = menuItems.indexOf(this.activeItem)
+          this.deactivate(menuItems)
+          this.activate(index + 1, 0)
+        } else if (e.key === "ArrowUp") {
+          let menuItems = this.menuItems()
+          let index = menuItems.indexOf(this.activeItem)
+          this.deactivate(menuItems)
+          this.activate(index - 1, menuItems.length - 1)
+        }
       }
     })
   },
-  activate(index, fallbackIndex){
+  activate(index, fallbackIndex) {
     let menuItems = this.menuItems()
     this.activeItem = menuItems[index] || menuItems[fallbackIndex]
     this.activeItem.classList.add(this.activeClass)
+    this.activeItem.focus()
   },
-  deactivate(items){ items.forEach(item => item.classList.remove(this.activeClass)) },
-  menuItems(){ return Array.from(this.menuItemsContainer.querySelectorAll("[role=menuitem]")) }
+  deactivate(items) { items.forEach(item => item.classList.remove(this.activeClass)) },
+  menuItems() { return Array.from(this.menuItemsContainer.querySelectorAll("[role=menuitem]")) }
 }
 
 Hooks.Flash = {
-	mounted(){
+  mounted() {
     let hide = () => this.el.click()
     let timer = setTimeout(() => hide(), 8000)
     this.el.addEventListener("mouseover", () => {
       clearTimeout(timer)
       timer = setTimeout(() => hide(), 8000)
     })
-	}
+  }
 }
 
 Hooks.AudioPlayer = {
-  mounted(){
+  mounted() {
     this.playbackBeganAt = null
     this.player = this.el.querySelector("audio")
     this.currentTime = this.el.querySelector("#player-time")
     this.duration = this.el.querySelector("#player-duration")
     this.progress = this.el.querySelector("#player-progress")
     let enableAudio = () => {
-      if(this.player.src){
+      if (this.player.src) {
         document.removeEventListener("click", enableAudio)
-        if(this.player.readyState === 0){
+        if (this.player.readyState === 0) {
           this.player.play().catch(error => null)
           this.player.pause()
         }
       }
     }
     document.addEventListener("click", enableAudio)
-    this.el.addEventListener("js:listen_now", () => this.play({sync: true}))
+    this.el.addEventListener("js:listen_now", () => this.play({ sync: true }))
     this.el.addEventListener("js:play_pause", () => {
-      if(this.player.paused){
+      if (this.player.paused) {
         this.play()
       }
     })
-    this.handleEvent("play", ({url, token, elapsed}) => {
+    this.handleEvent("play", ({ url, token, elapsed }) => {
       this.playbackBeganAt = nowSeconds() - elapsed
       let currentSrc = this.player.src.split("?")[0]
-      if(currentSrc === url && this.player.paused){
-        this.play({sync: true})
-      } else if(currentSrc !== url) {
+      if (currentSrc === url && this.player.paused) {
+        this.play({ sync: true })
+      } else if (currentSrc !== url) {
         this.player.src = `${url}?token=${token}`
-        this.play({sync: true})
+        this.play({ sync: true })
       }
     })
     this.handleEvent("pause", () => this.pause())
     this.handleEvent("stop", () => this.stop())
   },
 
-  play(opts = {}){
-    let {sync} = opts
+  play(opts = {}) {
+    let { sync } = opts
     this.player.play().then(() => {
-      if(sync){ this.player.currentTime = nowSeconds() - this.playbackBeganAt }
+      if (sync) { this.player.currentTime = nowSeconds() - this.playbackBeganAt }
       this.progressTimer = setInterval(() => this.updateProgress(), 100)
     }, error => {
-      if(error.name === "NotAllowedError"){
+      if (error.name === "NotAllowedError") {
         execJS("#enable-audio", "data-js-show")
       }
     })
   },
 
-  pause(){
+  pause() {
     clearInterval(this.progressTimer)
     this.player.pause()
   },
 
-  stop(){
+  stop() {
     clearInterval(this.progressTimer)
     this.player.pause()
     this.player.currentTime = 0
@@ -137,29 +138,29 @@ Hooks.AudioPlayer = {
     this.currentTime.innerText = ""
   },
 
-  updateProgress(){
-    if(isNaN(this.player.duration)){ return false }
-    if(this.player.currentTime >= this.player.duration){
+  updateProgress() {
+    if (isNaN(this.player.duration)) { return false }
+    if (this.player.currentTime >= this.player.duration) {
       this.pushEvent("next_song_auto")
       clearInterval(this.progressTimer)
       return
     }
-		this.progress.style.width = `${(this.player.currentTime / (this.player.duration) * 100)}%`
+    this.progress.style.width = `${(this.player.currentTime / (this.player.duration) * 100)}%`
     this.duration.innerText = this.formatTime(this.player.duration)
     this.currentTime.innerText = this.formatTime(this.player.currentTime)
   },
 
-  formatTime(seconds){ return new Date(1000 * seconds).toISOString().substr(14, 5) }
+  formatTime(seconds) { return new Date(1000 * seconds).toISOString().substr(14, 5) }
 }
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {
-	hooks: Hooks,
-  params: {_csrf_token: csrfToken}
+  hooks: Hooks,
+  params: { _csrf_token: csrfToken }
 })
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
 window.addEventListener("phx:page-loading-start", info => topbar.show())
 window.addEventListener("phx:page-loading-stop", info => topbar.hide())
 
