1673d90e940e6fdfa0f6c9fc2b2557a55388e638
Chris McCord <chris@chrismccord.com>
2021-12-03 08:56:33 -0500

    Fix deps

--- mix.exs
+++ mix.exs
@@ -33,8 +33,7 @@ defmodule LiveBeats.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      # {:phoenix, "~> 1.6.0"},
-      {:phoenix, path: "~/oss/phoenix", override: true},
+      {:phoenix, "~> 1.6.0"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
