ce1a1fdc5f58293724b4e6ba2c2dd0c1b98334f0
Chris McCord <chris@chrismccord.com>
2021-12-15 12:48:05 -0500

    Do not focus outline h1 in title bar

--- lib/live_beats_web/live/live_helpers.ex
+++ lib/live_beats_web/live/live_helpers.ex
@@ -448,7 +448,7 @@ defmodule LiveBeatsWeb.LiveHelpers do
     <!-- Page title & actions -->
     <div class="border-b border-gray-200 px-4 py-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8 h-16">
       <div class="flex-1 min-w-0">
-        <h1 class="text-lg font-medium leading-6 text-gray-900 sm:truncate">
+        <h1 class="text-lg font-medium leading-6 text-gray-900 sm:truncate focus:outline-none">
           <%= render_slot(@inner_block) %>
         </h1>
       </div>
