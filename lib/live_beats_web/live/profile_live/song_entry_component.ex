58f792a97304ed0ba24cd8af32bb1d4f1f9cd611
Nolan Darilek <nolan@thewordnerd.info>
2021-12-15 15:11:19 -0600

    Set autofocus on title element to grab focus on upload completion.

--- lib/live_beats_web/live/profile_live/song_entry_component.ex
+++ lib/live_beats_web/live/profile_live/song_entry_component.ex
@@ -22,7 +22,8 @@ defmodule LiveBeatsWeb.ProfileLive.SongEntryComponent do
           <% end %>
         </label>
         <input type="text" name={"songs[#{@ref}][title]"} value={@title}
-          class="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"/>
+          class="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
+          autofocus/>
       </div>
       <div class="border border-gray-300 rounded-md px-3 py-2 mt-2 shadow-sm focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600">
         <label for="name" class="block text-xs font-medium text-gray-900">Artist</label>
