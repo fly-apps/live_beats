0b00f87d614460c26f8dcac532495ef52edac2d3
Garrett George <garrett@verypossible.com>
2023-03-16 18:48:11 -0500

    WIP: Build Hello image in CI

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "LiveBeats";
+  description = "Play music together with Phoenix LiveView!";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
@@ -11,12 +11,33 @@
       let
         # pkgs = nixpkgs.legacyPackages.${system};
         pkgs = import nixpkgs { inherit system; };
+        imagePkgs = import nixpkgs { system = "x86_64-linux"; };
 
       in
       rec {
         packages = {
           live-beats = pkgs.callPackage ./pkgs/live-beats { inherit pkgs self; };
           default = packages.live-beats;
+
+          image = pkgs.dockerTools.buildImage {
+            name = "hello";
+            tag = "latest";
+            created = "now";
+            copyToRoot = pkgs.buildEnv {
+              name = "image-root";
+              paths = [ pkgs.hello ];
+              pathsToLink = [ "/bin" ];
+            };
+
+            config.Cmd = [ "/bin/hello" ];
+          };
+
+          layeredImage = pkgs.dockerTools.buildLayeredImage {
+            name = "hello";
+            config = {
+              Cmd = [ "${pkgs.hello}/bin/hello" ];
+            };
+          };
         };
 
 
@@ -25,6 +46,7 @@
 
           dev = import ./pkgs/dev-shell {
             inherit pkgs;
+            inputsFrom = [ packages.live-beats ];
             db_name = "db_dev";
             # MIX_ENV = "dev";
           };
